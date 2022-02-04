a57c4c6497a10b681e1d15046811b17958251b6b
mohamed muflahi <muflahimohamed88@gmail.com>
2022-02-04 10:33:53 -0500

    commit tree 1439d3046ab90d8771f51906cb741df50bcc186d

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () =>{
+    console.log("The Dom had Loaded");
+    const text = document.getElementById("text");
+    text.innerHTML = "This is really cool!";
+});
